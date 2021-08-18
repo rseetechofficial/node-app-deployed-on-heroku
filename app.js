964da2dbb3574b9b170b89d5e54546e063588a42
ramesh kumar <ramesh.comx@gmail.com>
2021-08-18 22:39:27 +0530

    first commit again

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const Joi = require('@hapi/joi');
+const movies = require('./movies');
+
+app.use(express.json());
+
+app.use('/abc', movies);
+
+
+app.get('/', (req,res) => {
+    res.send('Welcome to Daily Code again');
+})
+
+
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
